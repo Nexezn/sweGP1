f4fac8401e26e3d45d8d5c121c7eb70fa6143a25
dbiv-39 <114115863+dbiv-39@users.noreply.github.com>
2023-10-20 11:02:36 -0500

    Updated URLS for Core App

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,10 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('privacy/', views.privacy, name='privacy'),
     path('signup/', views.signup, name='signup'),
+    path('termsOfUse/', views.termsOfUse, name='termsOfUse'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ]
