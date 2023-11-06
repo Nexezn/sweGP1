6c495aa1385e31e64a9bac1ed69ccabc57ffabe0
dbiv-39 <114115863+dbiv-39@users.noreply.github.com>
2023-11-06 13:17:52 -0600

    Updated models.py

--- item/models.py
+++ item/models.py
@@ -11,11 +11,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+#added
 class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    quantity = models.IntegerField(default=1)
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
